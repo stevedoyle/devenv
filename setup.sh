b86c6893973207bdb63a9d802a940e0e74340ac0
stevedoyle <stephendoyle75@gmail.com>
2016-10-17 10:49:32 +0100

    Adding clang setup for YCM

--- setup.sh
+++ setup.sh
@@ -10,5 +10,12 @@
 echo "Installing Vundle plugins for vim ..."
 vim +PluginInstall +qall
 
+if test -e /usr/bin/dnf; then
+	dnf install -y python-devel cmake
+	cd ~/.vim/bundle/YouCompleteMe
+	./install.py --clang-completer
+	cd -
+fi
+
 # ctags setup
 \cp -v --backup=numbered dotctags ~/.ctags
